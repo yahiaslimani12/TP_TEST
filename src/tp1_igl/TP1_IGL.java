a750bc95c9827a2226c0589a34aa0b0c71412e39
Bes <Bes@DESKTOP-TEAG1GH>
2017-10-13 21:27:20 +0100

    dsfdsf

---
+++ src/tp1_igl/TP1_IGL.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1_igl;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Bes
+ */
+public class TP1_IGL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws TaillesDifferentesException {
+    ArrayList<Integer> Vecteur1 = new ArrayList<>();
+    ArrayList<Integer> Vecteur2 = new ArrayList<>();
+    ArrayList<Integer> Vecteur3 = new ArrayList<>();
+    VectorHelper helper = new VectorHelper();
+    
+    Vecteur1.add(5);
+     Vecteur1.add(10);
+      Vecteur1.add(7);
+       Vecteur1.add(12);
+        Vecteur1.add(2);
+Vecteur1.add(15);
+    Vecteur2.add(5);
+     Vecteur2.add(10);
+      Vecteur2.add(7);
+       Vecteur2.add(12);
+        Vecteur2.add(2);
+        
+    
+    
+    }
+    
+}
