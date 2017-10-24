8f34e4b5ba68e9273fcf11b0fa2a2efe0d74be7f
Bes <Bes@DESKTOP-TEAG1GH>
2017-10-24 19:28:32 +0100

    TP igl

--- src/tp1_igl/TP1_IGL.java
+++ src/tp1_igl/TP1_IGL.java
@@ -17,23 +17,6 @@ public class TP1_IGL {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws TaillesDifferentesException {
-    ArrayList<Integer> Vecteur1 = new ArrayList<>();
-    ArrayList<Integer> Vecteur2 = new ArrayList<>();
-    ArrayList<Integer> Vecteur3 = new ArrayList<>();
-    VectorHelper helper = new VectorHelper();
-    
-    Vecteur1.add(5);
-     Vecteur1.add(10);
-      Vecteur1.add(7);
-       Vecteur1.add(12);
-        Vecteur1.add(2);
-Vecteur1.add(15);
-    Vecteur2.add(5);
-     Vecteur2.add(10);
-      Vecteur2.add(7);
-       Vecteur2.add(12);
-        Vecteur2.add(2);
-        
     
     
     }
